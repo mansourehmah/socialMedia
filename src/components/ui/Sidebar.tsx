c2a809a4423c4aa39645e32c818d37be534a6e93
Mohammad Reza <attarmohammadreza83@gmail.com>
2026-05-09 12:21:41 +0330

    sidebar profile and username suggestion in signin

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,21 +1,98 @@
 import { useSession } from "../../stores";
-
-const AuthenticatedSidebar = () => {
-  return <div>yes login :) </div>;
-};
-
-const NotAuthenticatedSidebar = () => {
-  return <div>no login</div>;
-};
+import { Link } from "react-router";
 
 export const Sidebar = () => {
   const { session } = useSession();
 
+  // اگر لاگین نیست → کارت خوش‌آمدگویی
+  if (!session) {
+    return (
+      <aside className="lg:col-span-3">
+        <div className="bg-[#fff] border border-[#E5E5E5] rounded-xl p-5 space-y-6 shadow-xl dark:bg-[#0A0A0A] dark:border-[#262626] hidden lg:block">
+          <h2 className="text-xl font-bold text-[#171717] dark:text-[#FAFAFA] text-center">
+            Welcome Back!
+          </h2>
+          <p className="text-[#737373] dark:text-[#A3A3A3] text-sm text-center">
+            Login to access your profile and connect with others.
+          </p>
+
+          <div className="flex flex-col gap-3 mt-4">
+            <Link
+              to="/sign-in"
+              className="w-full text-center py-2 rounded-md bg-[#0A0A0A] text-white font-medium shadow-lg dark:border dark:border-[#262626]"
+            >
+              Log In
+            </Link>
+
+            <Link
+              to="/sign-up"
+              className="bg-[#FAFAFA] w-full text-center py-2 rounded-md border border-white text-[#171717] font-medium shadow-lg"
+            >
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </aside>
+    );
+  }
+
+  // اگر لاگین شده → کارت پروفایل
+  const user = session.user;
+
+  const avatar = user.image ? user.image : user.name?.charAt(0)?.toUpperCase();
+
   return (
-    <div className="hidden lg:block lg:col-span-3">
-      <div className="border">
-        {session ? <AuthenticatedSidebar /> : <NotAuthenticatedSidebar />}
+    <aside className="lg:col-span-3">
+      <div className="bg-white dark:bg-[#0A0A0A] border border-[#E5E5E5] rounded-xl p-5 space-y-6 hidden lg:block">
+        {/* Avatar */}
+        <div className="flex justify-center">
+          {user.image ? (
+            <img
+              src={user.image}
+              alt="profile"
+              className="w-24 h-24 rounded-full object-cover"
+            />
+          ) : (
+            <div className="w-24 h-24 rounded-full bg-[#222] flex items-center justify-center text-4xl font-bold text-white">
+              {avatar}
+            </div>
+          )}
+        </div>
+
+        {/* User Info */}
+        <div className="text-center space-y-1">
+          <h2 className="text-lg font-bold text-[#171717] dark:text-[#FAFAFA]">
+            {user.name}
+          </h2>
+          <p className="text-[#737373] dark:text-[#A3A3A3]">
+            {user.email?.split("@")[0]}
+          </p>
+        </div>
+        <div className="h-px bg-[#E5E5E5] dark:bg-[#262626]"></div>
+
+        {/* Stats */}
+        <div className="flex justify-center gap-20 text-sm xl:gap-35">
+          <div className="text-center">
+            <p className="text-[#171717] dark:text-[#FAFAFA] font-semibold">
+              {user.following ?? 0}
+            </p>
+            <p className="text-[#737373] dark:text-[#A3A3A3]">Following</p>
+          </div>
+
+          <div className="text-center">
+            <p className="text-[#171717] dark:text-[#FAFAFA] font-semibold">
+              {user.followers ?? 0}
+            </p>
+            <p className="text-[#737373] dark:text-[#A3A3A3]">Followers</p>
+          </div>
+        </div>
+        <div className="h-px bg-[#E5E5E5] dark:bg-[#262626]"></div>
+        {/* Extra Info */}
+        <div className="space-y-2 text-sm text-[#737373] dark:text-[#A3A3A3]">
+          <p>📍 {user.location ?? "No location"}</p>
+          <p>🔗 {user.website ?? "No website"}</p>
+        </div>
       </div>
-    </div>
+    </aside>
   );
 };
